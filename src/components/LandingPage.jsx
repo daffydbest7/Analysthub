9293b9a565b3fce64a2fd69778d6a284cb754259
daffybest7 <daffydbest@gmail.com>
2024-07-16 11:27:30 +0100

    enum removal

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -19,7 +19,7 @@ const LandingPage = () => {
         <p className="mt-2 md:mt-4 lg:mt-4 font-semibold text-neutral-800 font-sans text-center">Welcome! Kindly Navigate using the buttons provided below.</p>
         <div className="mt-4 md:mt-8 lg:mt-8 mx-auto flex flex-row space-x-8  justify-center">
           <div>
-            <Link to="/auth/waec_capture">
+            <Link to="">
               <button className="px-4 py-2 rounded-lg bg-mainColor hover:bg-blue-600 text-white font-sans font-bold"> WAEC </button>
             </Link>
           </div>
